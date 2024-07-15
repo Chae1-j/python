ebb53dc3018375af5a4c72defe15dfb019276ffb
Chaewon Jeong <jcw3241@gmail.com>
2024-07-15 23:41:13 +0900

    0715 파이썬 강의정리

---
+++ Python_practices/ex07.py
@@ -0,0 +1,31 @@
+import sys
+# ㅍㅏ일 입출력
+
+
+
+
+# ------------------------------------------------------- 실습 ------------------------------------------------------
+# 표준 출력을 파일로 변경하여 구구단을 파일에 저장하는 코드 작성
+for i in range(2,10):
+    for j in range(1,10):
+        print("{} X {} = {}".format(i,j,i*j))
+
+f = open('b.text')
+
+# 2. 바탕화면에 python 폴더 생성 후 파일 생성
+"""
+txt = "Hello, World"
+
+f = open('test.txt', 'w')
+f.write(txt)
+f.close 
+
+import os
+os.mkdir('test') # 기본 경로에 생성됨
+os.getcwd()
+
+os.mkdir('바탕화면 경로 적기~')
+r = open('절대경로 입력', 'w')
+os.chdir()
+
+"""
