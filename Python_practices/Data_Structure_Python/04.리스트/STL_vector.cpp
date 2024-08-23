3eb5e997f11479615df38ed14559fc50d5e5e0d4
jcw3241 <jcw3241@gmail.com>
2024-08-23 11:16:17 +0900

    0823 자료구조 - 리스트(연결구조/ 배열구조) c++샘플용 업로드

---
+++ Python_practices/Data_Structure_Python/04.리스트/STL_vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+
+ostream & oprator << (ostream& os, vector<T>& v) {
+    os << "<";
+    for(int i = 0; i < (int)v.size(); i++) 
+        os << v[i] << " ";
+    os << ">";
+    return os;
+}
+
+int main() {
+    vector<int> vec(10);
+    for(int i = 0; i < (int)vec.size(); i++)
+        vec[i] = rand() % 100;
+
+    count << "Before: " << vec << endl;
+
+    for(auto it = vec.begin(); it != vec.end(); ) {
+        if(*it % 2 == 0)
+            it = vec.erase(it);
+        else ++it;
+    
+    }
+
+    cout << "After : " << vec << endl;
+    return 0;
+}
