c211d49f5f99505c6a63724b37d98d6ce4ff55a8
Chaewon Jeong <jcw3241@gmail.com>
2024-08-24 23:56:35 +0900

    0824 자료구조4- 파이썬 실습(리스트)

--- Python_practices/Data_Structure_Python/04.리스트/list_basic.py
+++ Python_practices/Data_Structure_Python/04.리스트/list_basic.py
@@ -15,3 +15,32 @@ for i in range(len(B)):
     sum = sum + B[i]
 
 print("sum(B) = ", sum)
+
+# 리스트 만들기 고급
+D = [5]*10
+print("D = ", D)
+
+# 리스트 내포(comprehension)
+import random
+E = [random.randint(0,10) for i in range(10)]
+print("E = ", E)
+
+# 리스트 얕은 복사
+F= 8
+print("E = ", E)
+print ("F =", F)
+F[3] = 1000
+print("E = ", E)
+print("F=", F)
+
+
+# 리스트 깊은 복사
+G = list(E)
+G[3] = 2000
+print("E = ", E)
+print("G =", G)
+
+# 슬라이스 기능
+H = G[0:8:2]
+print("G = ", G)
+print("H = ", H)
