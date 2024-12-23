9efd53e46553c46c18577b9e43c07c7293eca829
chaewon <jcw3241@gmail.com>
2024-12-23 13:00:09 +0900

    1223 힙정렬 실습코드 추가

--- Python_practices/Data_Structure_Python/17.다양한 정렬전략/힙정렬.py
+++ Python_practices/Data_Structure_Python/17.다양한 정렬전략/힙정렬.py
@@ -9,18 +9,58 @@
 - 입력 리스트를 이용해 정렬 
 1. 정렬되지 않은 리스트 > 최대 힙 : 
     - heapify
-    - 리스트의 앞 쪽 절반만 처리
+    - 리스트의 앞 쪽 절반만 처리 (뒤쪽 절반은 가족관계가 아니므로)
     - downHeap을 통해 최대 힙 조건 유지
 2. 최대 힙 > 정렬 리스트트
+    - 힙의 루트를 반복적으로 꺼냄 > 최댓값
+    - 최댓값을 배열의 맨 뒤쪽부터 저장
+    - 최대 힙의 크기가 하나 줄어듦듦
 
 '''
 
-def heapSort(data) :
-    heap = MinHeap()
-    for n in data :
-        heap.insert(n)
-    for i in range(len(data)):
-        data[i] = heap.delete()
+def heapify(arr, n, i) :
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    
+    # 더 큰 자식을 largest로 변경
+    if l < n and arr[i] < arr[l] : largest = l
+    if r < n and arr[largest] < arr[r] : largest = r
+    
+    if largest != i :                               # largest가 바뀌었으면 -> 교환, 순환호출
+        arr[i],arr[largest] = arr[largest], arr[i] # swap
+        heapify(arr, n, largest)
+
+# def heapSort(data) :
+#     heap = MinHeap()
+#     for n in data :
+#         heap.insert(n)
+#     for i in range(len(data)):
+#         data[i] = heap.delete()
+
+def heapSort(arr) :
+    n = len(arr)
+    
+    # 정렬되지 않은 배열 > 최대 힙
+    print("i=", 0, arr)
+    for i in range(n//2, -1, -1):
+        heapify(arr,n,i)
+        print("i=", i, arr)
+    print()
+    
+    # 최대 힙 > 정렬된 배열열
+    for i in range(n-1, 0, -1) :
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i , 0)
+        print("i=", i, arr)
         
-def MinHeap() :
-    print("")
+arr = [ 5, 3, 8, 4, 9, 1, 6, 2, 7]
+heapSort(arr)
+n = len(arr)
+print("HeapSort: " , arr)
+
+
+import heapq
+
+arr = [ 5, 3, 8, 4, 9, 1, 6, 2, 7 ]
+print()
